733146d45254a86d69e3fff7a09a1a9838529be4
Xander Azuaje <charon-mortis@pm.me>
2024-08-24 11:43:45 +0200

    ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,3 +1,21 @@
 //
 // Created by xander on 8/23/24.
 //
+#include <iostream>
+
+int main(int argc, char * argv[])
+{
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	for (int i = 1; i < argc; i++) {
+		std::string str(argv[i]);
+		for (size_t j = 0; j < str.length(); j++) {
+			std::cout << static_cast<char>(std::toupper(str[j]));
+		}
+		std::cout << " ";
+	}
+	std::cout << std::endl;
+	return 0;
+}
